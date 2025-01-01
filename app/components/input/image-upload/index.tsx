d98a60204a8ab48bb333afacf216d36015d9963e
Muhammad Zain Faraz <bscs21083@itu.edu.pk>
2025-01-01 21:14:24 +0500

    Fixing listing issue

--- app/components/input/image-upload/index.tsx
+++ app/components/input/image-upload/index.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
-import { useCallback } from "react";
 import { TbPhotoPlus } from "react-icons/tb";
-
-declare global {
-  var cloudinary: any;
-}
 
 interface ImageUploadProps {
   onChange: (value: string) => void;
@@ -15,58 +9,40 @@ interface ImageUploadProps {
 }
 
 const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
-  const handleUpload = useCallback(
-    (result: any) => {
-      onChange(result.info.secure_url);
-    },
-    [onChange]
-  );
+  const defaultImage = "http://pix4.agoda.net/hotelimages/6/6/6_1112201742005261861.jpg?s=312x";
 
   return (
-    <CldUploadWidget
-      onUpload={handleUpload}
-      uploadPreset="lvoqqpi0"
-      options={{
-        maxFiles: 1,
-      }}
+    <div
+      className="
+        relative
+        flex
+        cursor-pointer
+        flex-col
+        items-center
+        justify-center
+        gap-4
+        border-2
+        border-dashed
+        border-neutral-300
+        p-20
+        text-neutral-600
+        transition
+        hover:opacity-70
+      "
     >
-      {({ open }) => {
-        return (
-          <div
-            onClick={() => open?.()}
-            className="
-              relative
-              flex
-              cursor-pointer
-              flex-col
-              items-center
-              justify-center
-              gap-4
-              border-2
-              border-dashed
-              border-neutral-300
-              p-20
-              text-neutral-600
-              transition
-              hover:opacity-70
-              "
-          >
-            <TbPhotoPlus size={50} />
-            <div className="text-lg font-semibold">Click to upload</div>
-            {value && (
-              <div className="absolute inset-0 h-full w-full">
-                <Image
-                  alt="Upload"
-                  fill
-                  style={{ objectFit: "cover" }}
-                  src={value}
-                />
-              </div>
-            )}
-          </div>
-        );
-      }}
-    </CldUploadWidget>
+      <TbPhotoPlus size={50} />
+      <div className="text-lg font-semibold">Click to upload</div>
+
+      {/* Display the default image */}
+      <div className="absolute inset-0 h-full w-full">
+        <Image
+          alt="Default Image"
+          fill
+          style={{ objectFit: "cover" }}
+          src={defaultImage}
+        />
+      </div>
+    </div>
   );
 };
 
